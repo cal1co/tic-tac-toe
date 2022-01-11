dcd9a1fa94c487524cd11de9b153a4149faf3391
Alex King <calicoalex@AlexnoMacBook-Pro.local>
2022-01-11 11:16:42 +1100

    colour work... starting user preference :)

--- js/main.js
+++ js/main.js
@@ -1,201 +1,184 @@
 // console.log('hello')
-//let players choose colours 
 xTurn = true;
 keepGoing = true;
-// winnerChosen = false;
-let colors = ["red", "blue", "green", "yellow", "orange"];
-
-let p1Color = "seagreen";
-let p2Color = "crimson";
-
-// $(".hovered").css("backgroundColor","green");    
-$(".cellButton").hover(
-    function () {
-        $(this).addClass('hovered')
-        $(".hovered").css("background-color", `${p1Color}`)
-        $(".hovered").html("X")
-    },
-    function () {
-        $(".hovered").css("background-color", `white`)
-        $(".hovered").html("")
-        $(this).removeClass('hovered')
-    }
-);
-
-$(".cellButton").on('click', function () {
-    // console.log($(this).parents(".cell").attr("id"));
-    let currentPick = $(this).parents(".cell").attr("id")
-    // console.log(this)
-    // $(this).html("X")
-
-    if (xTurn) {
-        $("#" + currentPick).html('<p id="p1">X</p>')
-        // $(this).html("X")
-        xTurn = false
-
-    } else {
-        $("#" + currentPick).html('<p id="p2">O</p>')
-        // $(this).html("O")
-        xTurn = true
-    }
-
-    if (xTurn === true) { // hover colour 
-        $(".cellButton").hover(
-            function () {
-                $(this).addClass('hovered')
-                $(".hovered").css("background-color", `${p1Color}`)
-                $(".hovered").html("X")
-            },
-            function () {
-                $(".hovered").css("background-color", `white`)
-                $(".hovered").html("")
-                $(this).removeClass('hovered')
-
-            }
-        );
-    } else if (xTurn === false) {
-        $(".cellButton").hover(
-            function () {
-                $(this).addClass('hovered')
-                $(".hovered").html("O")
-                $(".hovered").css("background-color", `${p2Color}`)
-            },
-            function () {
-                $(".hovered").css("background-color", `white`)
-                $(".hovered").html("")
-                $(this).removeClass('hovered')
-            }
-        );
-    };
 
 
-    if (keepGoing === true) {
-        if ($("#c1").children().attr("id") == $("#c2").children().attr("id") && $("#c1").children().attr("id") == $("#c3").children().attr("id")) {
-            if ($("#c1").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+if (cellSelector === false) {
+    $(".cellButton").on('click', function () {
+        console.log(cellSelector)
+        let currentPick = $(this).parents(".cell").attr("id") // the button pressed
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
-            }
-            keepGoing = false;
-            // console.log(keepGoing)
+        if (xTurn) {
+            $("#" + currentPick).html('<p id="p1">X</p>')
+            // $(this).html("X")
+            xTurn = false
+
+        } else {
+            $("#" + currentPick).html('<p id="p2">O</p>')
+            // $(this).html("O")
+            xTurn = true
         }
 
-        if ($("#c4").children().attr("id") == $("#c5").children().attr("id") && $("#c4").children().attr("id") == $("#c6").children().attr("id")) {
-            if ($("#c4").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+        if (xTurn) { // hover colour 
+            $(".cellButton").hover(
+                function () {
+                    $(this).addClass('hovered')
+                    $(".hovered").css("background-color", `${p1Color}`)
+                    $(".hovered").html("X")
+                },
+                function () {
+                    $(".hovered").css("background-color", `white`)
+                    $(".hovered").html("")
+                    $(this).removeClass('hovered')
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+            );
+        } // hover colour change p1
+        if (xTurn === false) {
+            $(".cellButton").hover(
+                function () {
+                    $(this).addClass('hovered')
+                    $(".hovered").html("O")
+                    $(".hovered").css("background-color", `${p2Color}`)
+                },
+                function () {
+                    $(".hovered").css("background-color", `white`)
+                    $(".hovered").html("")
+                    $(this).removeClass('hovered')
+                }
+            );
+        }; // hover colour change p2
+
+
+        if (keepGoing) {
+            if ($("#c1").children().attr("id") == $("#c2").children().attr("id") && $("#c1").children().attr("id") == $("#c3").children().attr("id")) {
+                if ($("#c1").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
+                // console.log(keepGoing)
             }
-            keepGoing = false;
-        }
 
-        if ($("#c7").children().attr("id") == $("#c8").children().attr("id") && $("#c7").children().attr("id") == $("#c9").children().attr("id")) {
-            if ($("#c7").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c4").children().attr("id") == $("#c5").children().attr("id") && $("#c4").children().attr("id") == $("#c6").children().attr("id")) {
+                if ($("#c4").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
-        }
 
-        if ($("#c1").children().attr("id") == $("#c4").children().attr("id") && $("#c4").children().attr("id") == $("#c7").children().attr("id")) {
-            if ($("#c1").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c7").children().attr("id") == $("#c8").children().attr("id") && $("#c7").children().attr("id") == $("#c9").children().attr("id")) {
+                if ($("#c7").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
-        }
 
-        if ($("#c2").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c8").children().attr("id")) {
-            if ($("#c2").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c1").children().attr("id") == $("#c4").children().attr("id") && $("#c4").children().attr("id") == $("#c7").children().attr("id")) {
+                if ($("#c1").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
-        }
 
-        if ($("#c3").children().attr("id") == $("#c6").children().attr("id") && $("#c6").children().attr("id") == $("#c9").children().attr("id")) {
-            if ($("#c3").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c2").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c8").children().attr("id")) {
+                if ($("#c2").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
-        }
 
-        if ($("#c1").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c9").children().attr("id")) {
-            if ($("#c1").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c3").children().attr("id") == $("#c6").children().attr("id") && $("#c6").children().attr("id") == $("#c9").children().attr("id")) {
+                if ($("#c3").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
-        }
 
-        if ($("#c3").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c7").children().attr("id")) {
-            if ($("#c3").children().attr("id") === "p1") {
-                $("body").append("X WINS!");
-                playerScoreX++
-                $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+            if ($("#c1").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c9").children().attr("id")) {
+                if ($("#c1").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
 
-            } else {
-                $("body").append('O WINS')
-                playerScoreY++
-                $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
             }
-            keepGoing = false;
+
+            if ($("#c3").children().attr("id") == $("#c5").children().attr("id") && $("#c5").children().attr("id") == $("#c7").children().attr("id")) {
+                if ($("#c3").children().attr("id") === "p1") {
+                    $(".board").append("<h2>X WINS!</h2>");
+                    playerScoreX++
+                    $("#p1Score").html(`P1 Score: ${playerScoreX}`)
+
+                } else {
+                    $(".board").append('<h2>O WINS!</h2>')
+                    playerScoreY++
+                    $("#p2Score").html(`P2 Score: ${playerScoreY}`)
+                }
+                keepGoing = false;
+            }
+            // $("#p1").css({"color": p1Color})
+            // $("#p2").css({"color": p2Color})
+            // $(#clickedCell).text("X")
+        } // closing keep going 
+        if (keepGoing === false) {
+            // console.log('tie')
+            $(".board").append("<h3>Game Over</h3>")
+            $(".cell").css({
+                "filter": 'blur(5px)',
+            });
         }
-        // $("#p1").css({"color": p1Color})
-        // $("#p2").css({"color": p2Color})
-        // $(#clickedCell).text("X")
-    } // closing keep going 
-    else if (keepGoing === false) {
-        // console.log('tie')
-        $("body").append("<h3>Game Over</h3>")
-    }
-}) //closing game
+    }) //closing game
+}
 
 let playerScoreX = localStorage.getItem("scoreX") || 0;
 let playerScoreY = localStorage.getItem("scoreY") || 0;
-
-
 
 $("#resetBoard").on('click', function () {
     localStorage.setItem("scoreY", playerScoreY)
@@ -203,23 +186,20 @@ $("#resetBoard").on('click', function () {
     window.location.reload()
 })
 
-
-// console.log("Player X has a score of " + localStorage.getItem("scoreX"))
-// console.log("Player Y has a score of " + localStorage.getItem("scoreY"))
-
 $("#clearBoard").on("click", function () {
     localStorage.clear("scoreX")
     localStorage.clear("scoreY")
     window.location.reload()
-
+    // console.log(p1Color)
 })
 
-$("body").append(`<p id="p1Score">P1 Score:${playerScoreX}`)
-$("body").append(`<p id="p2Score">P2 Score:${playerScoreY}`)
+$("body").append(`<h4 id="p1Score">P1 Score: ${playerScoreX}`)
+$("body").append(`<h4 id="p2Score">P2 Score: ${playerScoreY}`)
+
+
 
 
 //TODO: 
-// Customise colour for player 
 // Customise image for player (let them import one)
 // Customise shape of input fields 
 // Try this but w a 2d array! Might be shorter !! 
@@ -227,4 +207,26 @@ $("body").append(`<p id="p2Score">P2 Score:${playerScoreY}`)
 
 
 
+// 2d array ?? <== put this in a separate file... Too crowded here ..
+// let board = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9],
+// ]
 
+// const outputRows = function(){
+//     for (let i=0; i<board.length; i++){
+//     console.log(board[i])
+//     }
+// }
+// outputRows();
+
+
+// const outputColumns = function(){
+//     for (let i = 0; i < board.length; i++){
+//         for (let j = 0; j < board.length; j++){
+//             // board[i][j]
+//         }
+//     }
+// }
+// outputColumns();
---
+++ js/playerCust.js
@@ -0,0 +1,106 @@
+$(window).on("load", function () {
+    $("#p2Score").css("color", p2Color);
+    $("#p1Score").css("color", p1Color);
+})
+
+
+$(".colorButton").on("click", function () {
+    if ($(this).parent().parent().attr("id") === "p1") {
+        p1Color = ($(this).attr("id"))
+        $("#p1Score").css("color", p1Color);
+    } else {
+        p2Color = ($(this).attr("id"))
+        $("#p2Score").css("color", p2Color);
+    }
+
+    $("#p1").css("color", p1Color)
+    $("#p2").css("color", p2Color)
+    localStorage.setItem("p1Color", p1Color)
+    localStorage.setItem("p2Color", p2Color)
+    // console.log(localStorage.getItem("p1Color"))
+    // console.log(localStorage.getItem("p2Color"))
+})
+
+let p1Color = localStorage.getItem("p1Color") || "seagreen";
+let p2Color = localStorage.getItem("p2Color") || "crimson";
+
+$(".cellButton").hover(
+    function () {
+        $(this).addClass('hovered')
+        $(".hovered").css("background-color", `${p1Color}`)
+        $(".hovered").html("X")
+    },
+    function () {
+        $(".hovered").css("background-color", `white`)
+        $(".hovered").html("")
+        $(this).removeClass('hovered')
+    }
+);
+
+// input username! 
+
+// $("#p1Avatar").on("change", function(){
+    $("body").append($("#p1Avatar").val())
+    
+    // console.log(document.getElementById("p1Avatar").files[0].name);
+    console.log($("#p1Avatar").val());
+$("#p1Avatar").on("change", function(){
+    console.log("img changed");
+    $("#firstPlayerAvatar").attr("src", $("#p1Avatar").val());
+    console.log($("#firstPlayerAvatar")); // img information
+});
+
+cellSelector = true;
+
+$("#firstMove").on("click", function(){
+    cellSelector = !cellSelector;
+    console.log(cellSelector)
+
+})
+
+
+// allow preferences ie set first pos if first player 
+
+// $("#p1Avatar").on("change", function(){
+//     const file = $("#p1Avatar").files
+//     if (file){
+//         $("#firstPlayerAvatar").src = URL.createObjectURL(file)
+//     }
+// });
+
+
+// $("#p1Avatar").on("change", function(){
+// console.log('hello')
+//     if (typeof (FilerReader) != "undefined"){//typeof tells value of operand 
+//         $("#firstPlayerAvatar").html("");
+//         $($(this)[0].files).each(function(){
+//             let uploadImg = $(this);
+//                 let reader = new FileReader();
+//                 reader.onload = function(ev){
+//                     const img = $("<img />")
+//                     img.attr("style", "width: 100px; height: 100px")
+//                     img.attr("src",e.target.result);
+//                     $("#firstPlayerAvatar").append(img);
+//                 }
+//                 reader.readAsDataURL(file[0])
+//         });
+//     }
+// })
+
+
+
+
+let p1Name = "";
+let p2Name = "";
+
+
+// let p1Color = "crimson"
+
+
+// console.log(localStorage.getItem("p1Color"))
+// console.log(localStorage.getItem("p2Color"))
+// // console.log(p1Color)
+// localStorage.getItem("p1Color");
+// localStorage.getItem("p2Color")
+
+
